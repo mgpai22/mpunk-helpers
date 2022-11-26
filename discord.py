2f48b5af6f49bda90f2259930b38668485d61490
mgpai <64056966+mgpai22@users.noreply.github.com>
2022-11-26 17:57:51 -0500

    updated discord noti

--- discord.py
+++ discord.py
@@ -1,17 +1,9 @@
-from discord_webhook import DiscordWebhook, DiscordEmbed
+import discord_notify as dn
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-webhookLink = os.getenv("WEBHOOK")
-webhook = DiscordWebhook(url=webhookLink)
+def discordCall(position, score):
+    obj = dn.Notifier(os.getenv("WEBHOOK"))
+    obj.send(f"``` Mpunk Status Update \n \n Line position is: {position} \n \n Score is: {score}```", print_message=False)
 
-
-# create embed object for webhook
-# you can set the color as a decimal (color=242424) or hex (color='03b2f8') number
-
-def discordCall(position, score):  # sends discord message of the NFT that has been sent
-    embed = DiscordEmbed(title='Mpunk Status Update', description=f'Line position is {position} \n \n Score is {score}', color='03b2f8')
-    # add embed object to webhook
-    webhook.add_embed(embed)
-    response = webhook.execute()
